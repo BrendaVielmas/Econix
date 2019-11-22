5f8e94efbbdd17c816c83d0a6d998f68dbac0aea
Brenda Vielmas <breen.vielmas@gmail.com>
2019-11-22 01:19:57 -0600

    se quita boton fb

--- src/main.js
+++ src/main.js
@@ -72,8 +72,8 @@ let uiConfig = {
 	signInSuccessUrl: 'muro.html',
 	signInOptions: [
 		// Leave the lines as is for the providers you want to offer your users.
-		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID
+		
 	],
 	// Terms of service url.
 	tosUrl: 'index.html',
